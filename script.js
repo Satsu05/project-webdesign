512942afaff1ff9a7034910d8ffd8a7bde9b4085
Satsu05 <fleurdalia05@hotmail.com>
2022-05-10 12:23:58 +0200

    responsive

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-backgroundCount = 0;
-currentFrame = 1;
-
-function restartTimer() {
-  timePrompt = setTimeout(function() {
-    changeImage();
-
-  }, 6000);
-}
-
-function changeImage() {
-  backgroundCount++;
-  imageUrl = "url('https://unsplash.it/1200/750/?random&" + backgroundCount + "')";
-
-  if (currentFrame == 1) {
-    currentName = "backgroundImage1";
-    hideName = "backgroundImage2";
-    currentFrame = 2;
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    currentName = "backgroundImage2";
-    hideName = "backgroundImage1";
-    currentFrame = 1;
+    x.className = "topnav";
   }
-  document.getElementById(currentName).style.backgroundImage = imageUrl;
-  document.getElementById(hideName).className = 'hide';
-  document.getElementById(currentName).className = 'show';
-
-  restartTimer();
 }
-restartTimer();
